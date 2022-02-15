2fc27af210fa81dbf20d4eb61f4e2716cf8441d5
amal kalim <amalkalim@gmail.com>
2022-02-15 23:40:06 +0100

    commit structure changement

--- micro-services/auth-service/src/models/User.js
+++ micro-services/auth-service/src/models/User.js
@@ -5,6 +5,15 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema(
   {
 
+    first_name: {
+      type: String,
+      lowercase: true,
+    },
+    last_name: {
+      type: String,
+      lowercase: true,
+    },
+
     email: {
       type: String,
       required: true,
@@ -18,20 +27,19 @@ const userSchema = new mongoose.Schema(
       required: true,
       max: 1024,
       minlength: 6
+    },
+
+    role: {
+      type: String,
+      required: true
     }
-    
+
+
+
   },
-  {
-    timestamps: true,
-  }
-);
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }); //automatically add while insert or update the object
 
 
-userSchema.statics.login = async function (email, password) {
 
-    
-  
-
-};
 
 module.exports = User = mongoose.model("user", userSchema)
